380f5b12740dde77fb2abbc342b81823afc61d42
MaxBased <hoseintahan84@gmail.com>
2024-04-14 14:03:05 +0330

    Update  main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_v2ray/flutter_v2ray.dart';
+import 'package:v2ray/bloc/auth/auth_bloc.dart';
 import 'package:v2ray/view/screen/home-screen.dart';
 import 'package:v2ray/view/screen/login-screen.dart';
 import 'package:v2ray/view/screen/veryfication-screen.dart';
@@ -9,7 +11,10 @@ import 'di/di.dart';
 
 void main() async {
   await getItInit();
-  runApp(const MyApp());
+  runApp(BlocProvider<AuthBloc>(
+    create: (context) => AuthBloc(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -26,7 +31,7 @@ class MyApp extends StatelessWidget {
           border: OutlineInputBorder(),
         ),
       ),
-      home: Scaffold(body: VeryficationScreen()),
+      home: Scaffold(body: VerificationScreen()),
     );
   }
 }
