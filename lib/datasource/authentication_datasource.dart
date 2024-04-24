4907da8b86419481bbecf03e633e97900829f4f0
MaxBased <hoseintahan84@gmail.com>
2024-04-24 13:29:28 +0330

    Update  authentication_datasource.dart

--- lib/datasource/authentication_datasource.dart
+++ lib/datasource/authentication_datasource.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:v2ray/di/di.dart';
@@ -5,35 +7,27 @@ import 'package:v2ray/di/di.dart';
 import '../utils/api_exceptions.dart';
 
 abstract class IAuthenticationDatasource {
-  Future<void> register(String phoneNumber, String macAddress);
-
-  Future<void> login(String phoneNumber, String macAddress);
+  Future<Map<String, dynamic>> login(String phoneNumber, String macAddress);
 }
 
 class AuthenticationRemote implements IAuthenticationDatasource {
   final Dio _dio = locator.get();
 
   @override
-  Future<void> login(String phoneNumber, String macAddress) async {
+  Future<Map<String, dynamic>> login(
+      String phoneNumber, String macAddress) async {
     try {
-      await _dio.post('',
+      var response = await _dio.post('/users/login',
           data: {'phoneNumber': phoneNumber, 'macAddress': macAddress});
+      return response.data;
     } on DioException catch (ex) {
-      throw ApiException(
-          code: ex.response!.statusCode, message: ex.response!.data['message']);
-    } catch (e) {
-      throw ErrorDescription('خطای نامشخص');
-    }
-  }
-
-  @override
-  Future<void> register(String phoneNumber, String macAddress) async {
-    try {
-      await _dio.post('',
-          data: {'phoneNumber': phoneNumber, 'macAddress': macAddress});
-    } on DioException catch (ex) {
-      throw ApiException(
-          code: ex.response!.statusCode, message: ex.response!.data['message']);
+      if (ex.error is SocketException) {
+        throw ApiException(code: 0, message: 'اتصال به اینترنت برقرار نیست');
+      } else {
+        throw ApiException(
+            code: ex.response!.statusCode,
+            message: ex.response!.data['message'] ?? ex.error);
+      }
     } catch (e) {
       throw ErrorDescription('خطای نامشخص');
     }
