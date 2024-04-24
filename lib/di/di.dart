950c3ba51174a6b3e630c9a6cb65eb7ba5dfa55e
MaxBased <hoseintahan84@gmail.com>
2024-04-24 13:28:39 +0330

    Update  di.dart

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -6,7 +6,8 @@ import 'package:v2ray/repository/authentication_repository.dart';
 final locator = GetIt.instance;
 
 Future<void> getItInit() async {
-  locator.registerSingleton<Dio>(Dio());
+  locator.registerSingleton<Dio>(
+      Dio(BaseOptions(baseUrl: 'http://192.168.1.104:3000')));
 
   locator
       .registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
