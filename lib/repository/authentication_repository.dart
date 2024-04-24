2bab548d689f265fe439505195141f0433c85269
MaxBased <hoseintahan84@gmail.com>
2024-04-24 13:29:13 +0330

    Update  authentication_repository.dart

--- lib/repository/authentication_repository.dart
+++ lib/repository/authentication_repository.dart
@@ -1,44 +1,26 @@
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:v2ray/datasource/authentication_datasource.dart';
 import 'package:v2ray/di/di.dart';
 
 import '../utils/api_exceptions.dart';
 
 abstract class IAuthRepository {
-  Future<Either<String, String>> register(
-      String phoneNumber, String macAddress);
-
-  Future<Either<String, String>> login(String phoneNumber, String macAddress);
+  Future<Either<String, Map<String,dynamic>>> login(String phoneNumber, String macAddress);
 }
 
 class AuthenticationRepository implements IAuthRepository {
   final IAuthenticationDatasource _datasource = locator.get();
 
   @override
-  Future<Either<String, String>> login(
+  Future<Either<String, Map<String,dynamic>>> login(
       String phoneNumber, String macAddress) async {
     try {
-      await _datasource.login(phoneNumber, macAddress);
-      return right('ورود موفقیت آمیز بود');
+      Map<String,dynamic> response = await _datasource.login(phoneNumber, macAddress);
+      return right(response);
     } on ApiException catch (ex) {
-      return left(ex.message ?? 'خطایی رخ داد');
+      return left(ex.message!);
     } catch (e) {
       return left(e.toString() ?? 'خطایی رخ داد');
-    }
-  }
-
-  @override
-  Future<Either<String, String>> register(
-      String phoneNumber, String macAddress) async {
-    try {
-      await _datasource.login(phoneNumber, macAddress);
-      return right('ثبت نام موفقیت آمیز بود');
-    } on ApiException catch (ex) {
-      return left(ex.message ?? 'خطایی رخ داد');
-    } catch (e) {
-      return left(e.toString());
     }
   }
 }
