20a21c344b4ae74e3ded467cbbbb6cecc1b35b10
MaxBased <hoseintahan84@gmail.com>
2024-05-07 21:16:06 +0330

    Update     login-screen.dart

--- lib/view/screen/login-screen.dart
+++ lib/view/screen/login-screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:v2ray/bloc/auth/auth_bloc.dart';
 
+import '../../core/function/get-imei.dart';
+
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
 
@@ -12,13 +14,14 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
           children: [
             const SizedBox(height: 40),
             const Center(
               child: Text(
-                'Hello,',
+                'Hello ,',
                 style: TextStyle(color: Colors.blue, fontSize: 26),
               ),
             ),
@@ -43,66 +46,40 @@ class LoginScreen extends StatelessWidget {
             ),
             BlocBuilder<AuthBloc, AuthState>(
               builder: (context, state) {
-                if (state is AuthLoadingState) {
-                  return ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                      minimumSize: const Size(200, 50),
-                    ),
-                    onPressed: null,
-                    child: const CircularProgressIndicator(),
-                  );
-                } else {
-                  return ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                      minimumSize: const Size(200, 50),
-                    ),
-                    onPressed: () async {
-                      await Permission.phone.request();
-                      String phoneNumber = phoneNumberController.text;
-                      String macAddress = await _getDeviceIMEINumber();
+                return ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary:const Color(0xff3D83FF),
+                    minimumSize: const Size(200, 50),
+                  ),
+                  onPressed: state is AuthLoadingState
+                      ? null
+                      : () async {
+                          await Permission.phone.request();
+                          String phoneNumber = phoneNumberController.text;
+                          String macAddress =
+                              await ImeiManager().getDeviceIMEINumber();
 
-                      BlocProvider.of<AuthBloc>(context).add(
-                        AuthLoginEvent(
-                          phoneNumber: phoneNumber,
-                          macAddress: macAddress,
+                          // start Event
+                          BlocProvider.of<AuthBloc>(context).add(
+                            AuthLoginEvent(
+                              phoneNumber: phoneNumber,
+                              macAddress: macAddress,
+                            ),
+                          );
+                          // end of Event
+                        },
+                  child: state is AuthLoadingState
+                      ? const CircularProgressIndicator()
+                      : const Text(
+                          'LOG IN',
+                          style: TextStyle(fontSize: 24),
                         ),
-                      );
-                    },
-                    child: const Text(
-                      'LOG IN',
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  );
-                }
+                );
               },
-            ),
-            SizedBox(height: 32),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-                minimumSize: const Size(200, 50),
-              ),
-              onPressed: () {
-                // Handle sign-up button press
-              },
-              child: const Text(
-                'SIGN UP',
-                style: TextStyle(fontSize: 24),
-              ),
             ),
           ],
         ),
       ),
     );
-  }
-
-  Future<String> _getDeviceIMEINumber() async {
-    try {
-      return await DeviceInformation.deviceIMEINumber;
-    } catch (e) {
-      return '';
-    }
   }
 }
