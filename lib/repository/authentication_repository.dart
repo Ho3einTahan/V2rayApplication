a2f9b11b039da5e38c0c50019cf07a0186e6f0cb
MaxBased <hoseintahan84@gmail.com>
2024-05-07 21:12:08 +0330

    Update    authentication_repository.dart

--- lib/repository/authentication_repository.dart
+++ lib/repository/authentication_repository.dart
@@ -1,23 +1,26 @@
 import 'package:dartz/dartz.dart';
 import 'package:v2ray/datasource/authentication_datasource.dart';
 import 'package:v2ray/di/di.dart';
-
 import '../utils/api_exceptions.dart';
 
 abstract class IAuthRepository {
-  Future<Either<String, Map<String,dynamic>>> login(String phoneNumber, String macAddress);
+  Future<Either<String, Map<String, dynamic>>> login(
+      String phoneNumber, String macAddress);
 }
 
 class AuthenticationRepository implements IAuthRepository {
   final IAuthenticationDatasource _datasource = locator.get();
 
   @override
-  Future<Either<String, Map<String,dynamic>>> login(
+  Future<Either<String, Map<String, dynamic>>> login(
       String phoneNumber, String macAddress) async {
     try {
-      Map<String,dynamic> response = await _datasource.login(phoneNumber, macAddress);
+      Map<String, dynamic> response =
+          await _datasource.login(phoneNumber, macAddress);
+      // handle Right
       return right(response);
     } on ApiException catch (ex) {
+      // handle Error
       return left(ex.message!);
     } catch (e) {
       return left(e.toString() ?? 'خطایی رخ داد');
