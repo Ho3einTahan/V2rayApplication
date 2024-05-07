c273412fa0a9f931e3fee3525bc39bbbc20bd7f1
MaxBased <hoseintahan84@gmail.com>
2024-05-07 21:11:11 +0330

    Update  auth_bloc.dart

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -5,6 +5,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:v2ray/core/class/model/MyPrefrences.dart';
 import 'package:v2ray/di/di.dart';
 import 'package:v2ray/repository/authentication_repository.dart';
 import 'package:v2ray/view/screen/home-screen.dart';
@@ -23,6 +24,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       var either = await _repository.login(event.phoneNumber, event.macAddress);
 
       either.fold(
+        // if error
         (message) {
           Get.snackbar(
             '',
@@ -36,13 +38,21 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           );
           emit(AuthErrorState(message: message));
         },
-        (message) {
+        // if ok
+        (message) async {
           emit(AuthResponseState());
-          print(message['address']);
+          bool isLogin = await MyPreferences.readUserIsLogin();
+
           List<String> configServer = [];
           configServer = message['address'].toString().split(',');
 
-          Get.to(HomeScreen(serverConfig: configServer));
+          Get.offUntil(
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(serverConfig: configServer),
+            ),
+            (route) => false,
+          );
+
           Get.snackbar(
             '',
             '',
@@ -52,6 +62,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
                     style: const TextStyle(fontWeight: FontWeight.w700))),
             backgroundColor: Colors.lightBlue,
           );
+          MyPreferences.writeUserIsLogin(isLogin: true);
+          MyPreferences.writePhoneNumberUser(phoneNumber: event.phoneNumber);
         },
       );
     });
