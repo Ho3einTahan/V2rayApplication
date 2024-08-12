a134818d819027a0ac9361ecfac63b28d16d3717
MaxBased <hoseintahan84@gmail.com>
2024-08-12 07:41:39 +0330

    Update                       main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_v2ray/flutter_v2ray.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:v2ray/bloc/auth/auth_bloc.dart';
 import 'package:v2ray/view/screen/home-screen.dart';
-import 'package:v2ray/view/screen/login-screen.dart';
-import 'package:v2ray/view/screen/veryfication-screen.dart';
+
 import 'core/class/model/MyPrefrences.dart';
 import 'di/di.dart';
 
@@ -35,16 +33,45 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: Scaffold(
-          body: FutureBuilder<bool>(
-        future: MyPreferences.readUserIsLogin(),
-        builder: (context, snapshot) {
-          if (snapshot.data == false) {
-            return LoginScreen();
-          } else {
-            return HomeScreen(serverConfig: const []);
-          }
-        },
-      ),),
+        bottomNavigationBar: Container(
+          alignment: Alignment.bottomCenter,
+          height: 75,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+            border: GradientBoxBorder(gradient: LinearGradient(begin: Alignment.topRight, colors: [Color(0xff23C95B), Color(0xff6F34FE)]), width: 3.5),
+          ),
+          child: Center(
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+              child: BottomAppBar(
+                shape: const CircularNotchedRectangle(),
+                notchMargin: 6.0,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    IconButton(icon: const Icon(Icons.home, size: 32), onPressed: () {}),
+                    IconButton(icon: const Icon(Icons.filter_list_alt, size: 32), onPressed: () {}),
+                    IconButton(icon: const Icon(Icons.add, size: 32), onPressed: () {}),
+                    IconButton(icon: const Icon(Icons.person, size: 32), onPressed: () {}),
+                    IconButton(icon: const Icon(Icons.settings, size: 32), onPressed: () {}),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: FutureBuilder<bool>(
+          future: MyPreferences.readUserIsLogin(),
+          builder: (context, snapshot) {
+            if (snapshot.data == false) {
+              return HomeScreen(serverConfig: const []);
+            } else {
+              return HomeScreen(serverConfig: const []);
+            }
+          },
+        ),
+      ),
     );
   }
 }
