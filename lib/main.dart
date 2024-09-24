3af20ac6fd9b2a97797d915cffb65e74868b1cd8
MaxBased <hoseintahan84@gmail.com>
2024-09-24 20:28:13 +0330

    Update      main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:v2ray/bloc/auth/auth_bloc.dart';
-import 'package:v2ray/view/screen/home-screen.dart';
+import 'package:v2ray/bloc/vpn/vpn_bloc.dart';
+import 'package:v2ray/core/constants/routes.dart';
+import 'package:v2ray/utils/app-them.dart';
+import 'package:v2ray/view/screen/login-screen.dart';
 
-import 'core/class/model/MyPrefrences.dart';
 import 'di/di.dart';
 
-void main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await getItInit();
-  MyPreferences.initPrefrences();
-  runApp(BlocProvider<AuthBloc>(
-    create: (context) => AuthBloc(),
-    child: const MyApp(),
-  ));
+  getItInit();
+  runApp(
+    MultiBlocProvider(providers: [
+      BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
+      BlocProvider<VpnBloc>(create: (context) => VpnBloc()),
+    ], child: Application()),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        inputDecorationTheme: const InputDecorationTheme(
-          border: OutlineInputBorder(),
-        ),
-      ),
+      getPages: AppRoutes.getPages,
+      theme: AppTheme(),
       home: Scaffold(
-        bottomNavigationBar: Container(
-          alignment: Alignment.bottomCenter,
-          height: 75,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-            border: GradientBoxBorder(gradient: LinearGradient(begin: Alignment.topRight, colors: [Color(0xff23C95B), Color(0xff6F34FE)]), width: 3.5),
-          ),
-          child: Center(
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-              child: BottomAppBar(
-                shape: const CircularNotchedRectangle(),
-                notchMargin: 6.0,
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    IconButton(icon: const Icon(Icons.home, size: 32), onPressed: () {}),
-                    IconButton(icon: const Icon(Icons.filter_list_alt, size: 32), onPressed: () {}),
-                    IconButton(icon: const Icon(Icons.add, size: 32), onPressed: () {}),
-                    IconButton(icon: const Icon(Icons.person, size: 32), onPressed: () {}),
-                    IconButton(icon: const Icon(Icons.settings, size: 32), onPressed: () {}),
-                  ],
-                ),
-              ),
-            ),
-          ),
+        backgroundColor: const Color(0xff000046),
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: [
+            // CountryScreen(),
+            // HomeScreen(serverConfig: []),
+            // SettingScreen(),`
+            // SplitTunnelingScreen(),
+            // ProfileScreen(),
+            LoginScreen(),
+            // NotificationScreen(),
+            // BuyAccountScreen(),
+          ],
         ),
-        body: FutureBuilder<bool>(
-          future: MyPreferences.readUserIsLogin(),
-          builder: (context, snapshot) {
-            if (snapshot.data == false) {
-              return HomeScreen(serverConfig: const []);
-            } else {
-              return HomeScreen(serverConfig: const []);
-            }
-          },
-        ),
+        // bottomNavigationBar: Container(
+        //   alignment: Alignment.bottomCenter,
+        //   height: 75,+
+
+        //   decoration: const BoxDecoration(
+        //     color: Color(0xff1D192B),
+        //     borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+        //     border: GradientBoxBorder(gradient: LinearGradient(begin: Alignment.topRight, colors: [Color(0xff23C95B), Color(0xff6F34FE)]), width: 3.5),
+        //   ),
+        //   child: Center(
+        //     child: ClipRRect(
+        //       borderRadius: const BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+        //       child: BottomAppBar(
+        //         shape: const CircularNotchedRectangle(),
+        //         notchMargin: 6.0,
+        //         color: const Color(0xff1D192B),
+        //         child: Row(
+        //           mainAxisSize: MainAxisSize.max,
+        //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //           children: <Widget>[
+        //             IconButton(icon: const Icon(Icons.home, size: 32), onPressed: () => _onItemTapped(0)),
+        //             IconButton(icon: const Icon(Icons.person, size: 32), onPressed: () => _onItemTapped(1)),
+        //             IconButton(icon: const Icon(Icons.settings, size: 32), onPressed: () => _onItemTapped(2)),
+        //           ],
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
