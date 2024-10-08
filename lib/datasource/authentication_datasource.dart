13bfa22e134071284f80178deb7f2a79e7079ae7
MaxBased <hoseintahan84@gmail.com>
2024-10-08 21:56:03 +0330

    Update        authentication_datasource.dart

--- lib/datasource/authentication_datasource.dart
+++ lib/datasource/authentication_datasource.dart
@@ -1,36 +1,26 @@
-import 'dart:io';
-
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:v2ray/core/function/handle-dio-error.dart';
 import 'package:v2ray/di/di.dart';
+
+import '../core/constants/const.dart';
+import '../core/constants/routes.dart';
 import '../utils/api_exceptions.dart';
 
 abstract class IAuthenticationDatasource {
-  Future<Map<String, dynamic>> login(String phoneNumber, String macAddress);
+  Future<Either<ApiException, Map<String, dynamic>>> login(String phoneNumber, String password, String macAddress);
 }
 
 class AuthenticationRemote implements IAuthenticationDatasource {
-
   final Dio _dio = locator.get();
 
   @override
-  Future<Map<String, dynamic>> login(
-      String phoneNumber, String macAddress) async {
+  Future<Either<ApiException, Map<String, dynamic>>> login(String phoneNumber, String password, String macAddress) async {
     try {
-      var response = await _dio.post('/users/login',
-          data: {'phoneNumber': phoneNumber, 'macAddress': macAddress});
-      return response.data;
-    } on DioException catch (ex) {
-      if (ex.error is SocketException) {
-        throw ApiException(code: 0, message: 'اتصال به اینترنت برقرار نیست');
-      } else {
-        throw ApiException(
-          code: ex.response!.statusCode,
-          message: ex.response!.data['message'] ?? ex.error,
-        );
-      }
+      var response = await _dio.post(Const.login, data: {'phoneNumber': phoneNumber, 'password': password, 'macAddress': macAddress});
+      return right(response.data);
     } catch (e) {
-      throw ErrorDescription('خطای نامشخص');
+      return left(handleDioError(e));
     }
   }
 }
