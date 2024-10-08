5282a59902a2c4ca21efd5f41dff78df0c7650cc
MaxBased <hoseintahan84@gmail.com>
2024-10-08 21:55:19 +0330

    Update          di.dart

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,17 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:v2ray/datasource/authentication_datasource.dart';
+import 'package:v2ray/datasource/vpn-datasource.dart';
 import 'package:v2ray/repository/authentication_repository.dart';
+import 'package:v2ray/repository/vpn-repository.dart';
 
 final locator = GetIt.instance;
 
 Future<void> getItInit() async {
-  locator.registerSingleton<Dio>(Dio(BaseOptions(
-      baseUrl: 'http://192.168.1.104:3000',
-      receiveTimeout: const Duration(seconds: 25))));
+  locator.registerSingleton<Dio>(Dio(BaseOptions(baseUrl: 'http://192.168.1.101:3000', connectTimeout: const Duration(seconds: 10), receiveTimeout: const Duration(seconds: 20))));
 
-  locator
-      .registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
+  locator.registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
 
   locator.registerFactory<IAuthRepository>(() => AuthenticationRepository());
+
+  locator.registerFactory<IVpnDataSource>(() => VpnRemote());
+
+  locator.registerFactory<IVpnRepository>(() => VpnRepository());
 }
