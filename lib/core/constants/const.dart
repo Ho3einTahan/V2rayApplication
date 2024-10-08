e194e0353c99da8d6498d43df84c936398d8a2b7
MaxBased <hoseintahan84@gmail.com>
2024-10-08 21:55:37 +0330

    Add     const.dart

---
+++ lib/core/constants/const.dart
@@ -0,0 +1,22 @@
+class Const {
+  //url & endPoint
+  static const String baseUrl = "https://hoseintahan.ir";
+  static const String noInternet = "/noInternet";
+  static const String login = "/api/login";
+  static const String servers = "/api/servers";
+  static const String pricing = "/api/pricing";
+
+  //const
+  static const String isLoggedIn = "isLoggedIn";
+  static const String userPhoneNumber = "userPhoneNumber";
+  static const String userPassword = "userPassword";
+  static const String userToken = "userToken";
+  static const String selectedServerIndex = "selectedServerIndex";
+  static const String selectedServerAddress = "selectedServerAddress";
+  static const String appTunnels = "appTunnels";
+  static const String startServiceDate = "startServiceDate";
+  static const String endServiceDate = "endServiceDate";
+  static const String userName = "userName";
+  static const String devices_multiUser = "devices_multiUser";
+  static const String countryName = "countryName";
+}
